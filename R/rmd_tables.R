72c0e04736296d85bbace94db12cbe6b576524fd
Vilmantas <GegznaV@gmail.com>
2017-12-23 19:41:28 +0200

    Tables modiefied

--- R/rmd_tables.R
+++ R/rmd_tables.R
@@ -22,10 +22,70 @@ rmd_table_3 <- function() {
     rs_enclose_first_row_with_lines(above = tbl_3)
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname rmd_tables
+rmd_table_4 <- function() {
+    rs_enclose_first_row_with_lines(above = tbl_3)
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname rmd_tables
+rmd_table_5 <- function() {
+    rs_enclose_first_row_with_lines(above = tbl_3)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 tbl_1 <- c(
 "
 
-: The name of the table.
+: The name of the simple table.
+
+Left-aligned      Centered column       Right-aligned
+--------------  -------------------  ----------------
+Row_1_Col_1        Row_1_Col_2          Row_1_Col_3
+
+Row_2_Col_1        Row_2_Col_2          Row_2_Col_3
+
+Row_3_Col_1        Row_3_Col_2          Row_3_Col_3
+
+")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+tbl_2 <- c(
+"
+
+: The name of the table without column names.
+
+--------------  -----------------  ----------------
+Row_1_Col_1        Row_1_Col_2          Row_1_Col_3
+
+Row_2_Col_1        Row_2_Col_2          Row_2_Col_3
+
+Row_3_Col_1        Row_3_Col_2          Row_3_Col_3
+--------------  -----------------  ----------------
+
+")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+tbl_3 <- c(
+"
+
+: The name of the multi-row table.
+
+-----------------------------------------------------
+Left-aligned      Centered column      Right-aligned
+---------------- -----------------  ----------------
+Row_1_Col_1        Row_1_Col_2         Row_1_Col_3
+
+Row_2_Col_1        Row_2_Col_2         Row_2_Col_3
+
+Row_3_Col_1        Row_3_Col_2         Row_3_Col_3
+-----------------------------------------------------
+
+")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+tbl_4 <- c(
+"
+
+: The name of the markdown-style table.
 
 | Auto-aligned  | Left-aligned  |   Centered    | Right-aligned |
 | ------------- | :------------ | :-----------: | ------------: |
@@ -34,33 +94,16 @@ tbl_1 <- c(
 |  Row_3_Col_1  |  Row_3_Col_2  |  Row_3_Col_3  |  Row_3_Col_4  |
 
 ")
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-tbl_2 <- c(
-"
-
-: The name of the multi-row table.
-
------------------------------------------------------
-Left-aligned      Centered column      Right-aligned
----------------- -----------------  ----------------
-   Row_1_Col_1     Row_1_Col_2        Row_1_Col_3
-
-   Row_2_Col_1     Row_2_Col_2        Row_2_Col_3
-
-   Row_3_Col_1     Row_3_Col_2        Row_3_Col_3
------------------------------------------------------
-
-")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-tbl_3 <- c(
+tbl_5 <- c(
 "
 
-:  The name of the grid table.
+:  The name of the grid-style table.
 
 +---------------+---------------+--------------------+
-| Auto-aligned  |  Centered     | Right-aligned      |
-+===============+:=============:+===================:+
+| Right-aligned |  Centered     |  Auto-aligned      |
++==============:+:=============:+====================+
 | Row_1_Col_1   | Row_1_Col_2   | - Row_1_Col_3a     |
 |               |               | - Row_1_Col_3b     |
 +---------------+---------------+--------------------+
